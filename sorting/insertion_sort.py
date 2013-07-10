db88d48de5bcdb78b4252f59597ec5903753138f
Alex Reyes <akosialexpo@gmail.com>
2013-07-11 02:14:27 +0800

    Added insertion sort part 1

---
+++ sorting/insertion_sort.py
@@ -0,0 +1,21 @@
+#!/bin/python
+
+# Head ends here
+def insertionSort(list):
+	for index in range(1,len(list)):
+		on_deck = list[index]
+		comparing = index - 1
+
+		while comparing >= 0 and on_deck < list[comparing] :
+			list[comparing+1] = list[comparing]
+			print ' '.join(str(num) for num in list)
+			list[comparing] = on_deck
+			comparing -= 1
+
+	print ' '.join(str(num) for num in list)
+	
+# Tail starts here
+
+m = input()
+ar = [int(i) for i in raw_input().strip().split()]
+insertionSort(ar)
